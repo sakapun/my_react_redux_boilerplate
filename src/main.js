d5510bb49b387ac2d7ed8e402d0bc0e1e1a7f220
sakamoto.m <sakamoto.m@water-cell.jp>
2017-03-09 17:32:06 +0900

    途中だけど雑にコミット

--- src/main.js
+++ src/main.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
 
-const Sppan = (props) => {
+/* Actionの実装 */
+
+// Action名の定義
+
+const initialState = {
+    value: null,
+}
+
+const store = createStore(formReducer, initialState);
+
+store.dispatch(actionCreators());
+
+store.subscribe(() => {
+    store.getState();
+});
+
+
+const Spaan = (props) => {
     return <span>
         eroge
     </span>
 }
 import Button from './components/Button/Button.js'
 
+function mapStateToProps(state) {
+    return {
+        value: state.value,
+    };
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        onClick(value){
+            dispatch(send(value));
+        },
+    };
+}
+
+
+const AppContrainer = connect(
+
+)(Button);
 
 ReactDOM.render(
     <div>
         <Button name="unkonko" />
-        <Sppan />
+        <Spaan />
     </div>,
     document.getElementById('app')
 );
