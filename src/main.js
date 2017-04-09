b5b4c719b04f61df491512012176c402be2bfe07
sakapun <sakapun5th@gmail.com>
2017-04-09 17:27:09 +0900

    eslintの調教 インデントはタブでしょやっぱ

--- src/main.js
+++ src/main.js
@@ -1,57 +1,55 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider, connect } from "react-redux";
-import * as Action from "./action";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import * as Action from './action';
+import Button from './components/Button/Button';
 /* Actionの実装 */
 
-function createReducer (initialState, handlers) {
-    return function reducer(state = initialState, action) {
-        if (handlers.hasOwnProperty(action.type)) {
-            return handlers[action.type](state, action);
-        } else {
-            return state;
-        }
-    }
+function createReducer(initialState, handlers) {
+	return function reducer(state = initialState, action) {
+		if (handlers.hasOwnProperty(action.type)) {
+			return handlers[action.type](state, action);
+		}
+		return state;
+	};
 }
 
 const initialState = {
-    value: "unkouno",
-    loops: ["hoge", "fuga", "piyo"],
+	value: 'unkouno',
+	loops: ['hoge', 'fuga', 'piyo'],
 };
 
 const formReducer = createReducer([], {
-    [Action.SEND]: (state, action) => {
-        return {...state, value: action.value};
-    }
+	[Action.SEND]: (state, action) => {
+		return { ...state, value: action.value };
+	}
 });
 
 const store = createStore(formReducer, initialState);
 
-import Button from "./components/Button/Button.js";
-
 function mapStateToProps(state) {
-    return {
-        value: state.value,
-        loops: state.loops,
-    };
+	return {
+		value: state.value,
+		loops: state.loops,
+	};
 }
 function mapDispatchToProps(dispatch) {
-    return {
-        onClick(value){
-            dispatch(Action.send(value));
-        },
-    };
+	return {
+		onClick(value) {
+			dispatch(Action.send(value));
+		},
+	};
 }
 
 const AppContainer = connect(
-    mapStateToProps,
-    mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps,
 )(Button);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <AppContainer />
-    </Provider>,
-    document.getElementById('app')
+	<Provider store={store}>
+		<AppContainer />
+	</Provider>,
+	document.getElementById('app'),
 );
